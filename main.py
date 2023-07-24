015e0dbef0b17bb96683eccaceda618322d14836
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-07-24 11:09:17 -0300

    Added "checkjsonlog" function.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import glob,os,json
 
-
-def createjsonlog(path):   
-    """Creates a json file with an array that contains the names of all the content in the given path."""
+def getdata(path):
     data = []
     for filename in glob.iglob(path + '**/**', recursive=True):
         data.append(filename)
+    return data
+
+def createjsonlog(path):   
+    """Creates a json file with an array that contains the names of all the content in the given path."""
     with open("file-log.json", "w") as write_file:
-        json.dump(data, write_file)
+        json.dump(getdata(path), write_file)
+
+
+def checkjsonlog(path):
+    """Compares the names of the files and folders in the path to the ones in the json log."""
+    with open("file-log.json", "r") as read_file:
+        jsondata = json.load(read_file)
+    localdata = getdata(path)
+    return True if localdata == jsondata else False
 
 
